c22be5f07c9c33142f863b31f249bbb10438d490
Alexis <lexadrian1@gmail.com>
2018-06-25 11:02:16 -0500

    HTML

---
+++ MyApp/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [
+    './app.component.css'
+  ]
+})
+export class AppComponent {
+  // Interpolacion
+  titulo = 'Web';
+  usuario = {
+    nombre: 'Adrian',
+    apellido: 'Eguez'
+  };
+  arregloNumeros = [5, 4, 3, 2, 1];
+  mostrarTitulo = true;
+
+  imagenUrl = "https://coryrylan.com/assets/images/cory-rylan-gde.png";
+  imagenWidth = 200;
+
+  html = `
+  <h1>HOLA</h1>
+  <p>Hola mundo</p>
+  <img src="${this.imagenUrl}" alt="">
+`;
+
+
+}
